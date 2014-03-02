d944573f04df930f08e179f5a66978aa6b820630
LNA <LaToyaNAllen@gmail.com>
2014-03-02 17:38:56 -0600

    returns total_sales_for_item_in_USD_after_bankers_rounding

--- international_trade/lib/sales_converter.rb
+++ international_trade/lib/sales_converter.rb
@@ -1,4 +1,66 @@
+$: << File.expand_path(File.dirname(__FILE__) + "/../data/")
+require 'csv'
+require 'nokogiri'
+
 class SalesConverter
-  def initialize(transaction_file, rate_file, transaction_to_convert)
+  attr_accessor :transactions
+
+  def initialize
+    @transactions = []
+    @rates = {}
+    @usd_transactions = []
+  end
+ 
+  def convert_csv
+    CSV.foreach('data/sample_trans.csv', :headers => true) do |row|
+      amount, currency = row['amount'].split
+      @transactions << { store: row['store'].to_sym, sku: row['sku'].to_sym, amount: amount, currency: currency.to_sym }
+    end
+    @transactions
+  end
+
+  def convert_rates
+    @rates = {}
+
+    Nokogiri::XML(File.open('data/rates.xml')).css('rate').each do |rate|
+     original_dollar_rate = rate.css('from').text.to_sym
+     @rates[original_dollar_rate] ||= [] # if key isn't assigned a value set it equal to []
+     @rates[original_dollar_rate] << [rate.css('to').text.to_sym, rate.css('conversion').text.to_f]
+    end
+  end
+
+  def output_transactions_for(sku_symbol)
+    @transactions.find_all {|t| t[:sku] == sku_symbol}
+  end
+
+  def convert_to_float
+    transactions = output_transactions_for(:DM1182)
+    transactions.each do |t|
+      t[:amount] = t[:amount].to_f
+    end
+  end
+
+  def convert_to_usd
+    transactions = convert_to_float
+
+    transactions.each do |transaction|
+      while transaction[:currency] != :USD 
+        transaction[:amount] = transaction[:amount] * @rates[transaction[:currency]][0][1]
+        transaction[:currency] =  @rates[transaction[:currency]][0][0] 
+      end
+      @usd_transactions << transaction
+    end
+    @usd_transactions
+  end
+
+  def total_sales_for_item_in_USD_after_bankers_rounding
+    total = @usd_transactions.inject(0) {|sum, hash| sum + hash[:amount]}
+    total = total.to_i
+    if total.even?
+      total 
+    else
+      total = (total - 0.5)
+    end
+    total
   end
 end
--- international_trade/spec/sales_converter_spec.rb
+++ international_trade/spec/sales_converter_spec.rb
@@ -1,4 +1,67 @@
-require 'SalesConverter'
+require 'sales_converter'
+require 'csv'
+require 'nokogiri'
+require 'bankers_rounding'
+
 
 describe "SalesConverter" do 
+  let (:sales_converter) {SalesConverter.new}
+
+  it "prints the csv tansactions" do 
+    transactions = sales_converter.convert_csv
+  end
+
+  it "prints rates.xml" do   
+    rates = sales_converter.convert_rates  
+  end
+
+  it "outputs the total" do 
+    sales_converter.convert_csv
+    sales_converter.convert_rates
+
+    sales_converter.output_transactions_for(:DM1182).should ==  [{:store=>:Yonkers, :sku=>:DM1182, :amount=>"19.68", :currency=>:AUD}, 
+                                                      {:store=>:Nashua,  :sku=>:DM1182, :amount=>"58.58", :currency=>:AUD},
+                                                      {:store=>:Camden,  :sku=>:DM1182, :amount=>"54.64", :currency=>:USD}]
+  end
+
+  it "converts the first transaction to CAD from USD" do
+    sales_converter.convert_csv
+    sales_converter.convert_rates
+    transactions = sales_converter.convert_to_usd
+
+    transactions[0][:amount].should == 20.013991247999996
+  end
+
+  it "converts the second transaction to CAD from USD" do
+    sales_converter.convert_csv
+    sales_converter.convert_rates
+    transactions = sales_converter.convert_to_usd
+
+    transactions[1][:amount].should == 59.574167038
+  end
+
+  it "converts the third transaction to CAD from USD" do
+    sales_converter.convert_csv
+    sales_converter.convert_rates
+    transactions = sales_converter.convert_to_usd
+
+    transactions[2][:amount].should == 54.64
+  end
+
+  it "returns all converted transactions" do 
+    sales_converter.convert_csv
+    sales_converter.convert_rates
+
+    sales_converter.convert_to_usd.should == [{:store=>:Yonkers, :sku=>:DM1182, :amount=>20.013991247999996, :currency=>:USD},
+                                              {:store=>:Nashua, :sku=>:DM1182, :amount=>59.574167038, :currency=>:USD},
+                                              {:store=>:Camden, :sku=>:DM1182, :amount=>54.64, :currency=>:USD}]
+
+  end
+
+  it "returns the total sales amout for item in USD" do 
+    sales_converter.convert_csv
+    sales_converter.convert_rates
+    sales_converter.convert_to_usd
+    sales_converter.total_sales_for_item_in_USD_after_bankers_rounding.should == 134
+  end
 end
