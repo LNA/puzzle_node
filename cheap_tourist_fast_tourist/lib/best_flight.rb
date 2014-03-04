998c931847ccd90a34cdc48fc9f4ad2430145a4e
LNA <LaToyaNAllen@gmail.com>
2014-03-04 07:41:44 -0600

    aded best flight files

--- cheap_tourist_fast_tourist/lib/best_flight.rb
+++ cheap_tourist_fast_tourist/lib/best_flight.rb
@@ -1,2 +1,16 @@
-Class BestFlight
+$: << File.expand_path(File.dirname(__FILE__) + "/../data/")
+
+class BestFlight
+  def initialize(sales_input_file)
+    @sales_input_file = sales_input_file
+  end
+
+  def read_file
+    File.open(@sales_input_file, "r") do |f|
+      f.each_line do |line|
+        puts line
+      end
+    end
+  end
+
 end
--- cheap_tourist_fast_tourist/spec/best_flight_spec.rb
+++ cheap_tourist_fast_tourist/spec/best_flight_spec.rb
@@ -1,1 +1,13 @@
-best_flight_spec.rb
+require 'best_flight'
+
+describe "BestFlight" do 
+  let (:best_flight) {BestFlight.new('data/sample_input.txt')}
+
+  it "prints the sample_input file" do 
+    best_flight.read_file
+  end
+
+  it "returns the first line of flight info" do 
+    best_flight.return_first_flight('data/sample_input.txt').should ==    "A B 09:00 10:00 100.00"
+  end
+end
