3b942b4b29d8e9424a821e08b1bd07c0ebaaaccc
LNA <LaToyaNAllen@gmail.com>
2014-03-04 15:59:58 -0600

    indirect_conversions specs green for sample data

--- international_trade/lib/currency_converter.rb
+++ international_trade/lib/currency_converter.rb
@@ -1,22 +1,30 @@
 class CurrencyConverter
-
   def initialize(rates)
     @rates = rates
   end
 
-  def conversion_rate(from, to)
+  def conversion_rate(from, to, partial_conversion_rates)
+    @partial_conversion_rates = partial_conversion_rates
     to = to.to_sym
     from = from.to_sym
 
     if @rates[from].has_key?(to)
-      converstion_rate = @rates[from][:CAD]
+      @partial_conversion_rates << @rates[from].values
     else
-      indirect_conversion(from, to)
+      indirect_conversion(from, to, @partial_conversion_rates)
     end
-    converstion_rate
+    @partial_conversion_rates.flatten.inject(:*)
   end
 
-  def indirect_conversion(from, to)
-    # ?
+  def indirect_conversion(from, to, partial_conversion_rates)
+    @middleman_conversion_country = @rates[:AUD].keys.first
+
+    @rates.each do |rate|
+      if rate[1].has_key?(@middleman_conversion_country)
+        @partial_conversion_rates << rate[1].values
+      end
+    end
+    @partial_conversion_rates
+    conversion_rate(@middleman_conversion_country, to, @partial_conversion_rates)
   end
 end
