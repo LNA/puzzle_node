f63b62fd79f914b678dbfbaeb1379c15a7a831b5
LNA <LaToyaNAllen@gmail.com>
2014-03-11 12:28:19 -0500

    returns true for mutual mentions between two users

--- micro_blog/lib/first_order_connection.rb
+++ micro_blog/lib/first_order_connection.rb
@@ -1,7 +1,32 @@
 $: << File.expand_path(File.dirname(__FILE__) + "/../data/")
 
 class FirstOrderConnection
+  attr_accessor :file, :relevant_tweets
+
   def initialize(file)
     @file = file 
+  end
+
+  def tweets_involving(user)
+    @file = File.readlines(@file)
+    r = Regexp.new user
+    @relevant_tweets = file.grep(r) 
+  end
+
+  def mutual_mentions_between(first_user, second_user)
+    individual_connections = []
+    r = Regexp.new second_user
+    
+    mutual_tweets = @relevant_tweets.grep(r)
+    split =  mutual_tweets.map {|word| word.split(' ')}
+    split.each do |tweet|
+      individual_connections << tweet.grep(r)*","
+    end
+
+    if individual_connections.include?(second_user + ":") && individual_connections.include?("@" + second_user)
+      true
+    else
+      false
+    end
   end
 end
--- micro_blog/spec/first_order_connection_spec.rb
+++ micro_blog/spec/first_order_connection_spec.rb
@@ -4,7 +4,18 @@ describe FirstOrderConnection do
   let (:first_order_connection) {FirstOrderConnection.new('data/sample_input.txt')}
 
   it "returns all connections for Alberta" do 
-    require 'pry'
-    binding.pry
+    first_order_connection.tweets_involving("alberta").should == ["alberta: @bob \"It is remarkable, the character of the pleasure we derive from the best books.\"\n",
+                                                                  "bob: \"They impress us ever with the conviction that one nature wrote and the same reads.\" /cc @alberta\n", 
+                                                                  "alberta: hey @christie. what will we be reading at the book club meeting tonight?\n", 
+                                                                  "christie: 'Every day, men and women, conversing, beholding and beholden.' /cc @alberta, @bob\n", 
+                                                                  "alberta: @duncan, hope you're bringing those peanut butter chocolate cookies again :D\n"] 
+
+  end
+
+  it "returns albertas mutual mentions" do 
+    first_order_connection.tweets_involving("alberta")
+    
+
+    first_order_connection.mutual_mentions_between("alberta", "bob").should == true
   end
 end
