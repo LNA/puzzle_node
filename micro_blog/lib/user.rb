8c5f519b2e5e3ee2d12173e12194a5cb299920bf
LNA <LaToyaNAllen@gmail.com>
2014-03-17 16:54:58 -0500

    added username spambot: to specs

--- micro_blog/lib/user.rb
+++ micro_blog/lib/user.rb
@@ -1,5 +1,6 @@
 class User
-  attr_accessor :name, :received_tweets, :sent_tweets
+  attr_accessor :name, :names_of_users_tweeted_to,
+                :received_tweets, :sent_tweets, :senders
   def initialize(name)
     @name = name
   end
@@ -12,10 +13,19 @@ class User
     @received_tweets = tweets.select {|tweet| tweet.receiver == name}
   end
 
-  def tweets_involving_user
-    tweets_involving_user = []
-    tweets_involving_user << @sent_tweets
-    tweets_involving_user << @received_tweets
-    tweets_involving_user.flatten
+  def users_tweeted_to
+    @names_of_users_tweeted_to = @sent_tweets.map {|tweet| tweet.receiver }
+    @names_of_users_tweeted_to.uniq
+  end 
+
+  def received_tweets_from
+    @names_of_users_received_teweets_from = @received_tweets.map {|tweet| tweet.sender }
+    @names_of_users_received_teweets_from.uniq
+  end
+
+  def interacting_with
+    @names_of_users_interacting_with = []
+    @names_of_users_interacting_with << @names_of_users_tweeted_to << @names_of_users_received_teweets_from 
+    @names_of_users_interacting_with.flatten
   end
 end
--- micro_blog/spec/user_spec.rb
+++ micro_blog/spec/user_spec.rb
@@ -3,10 +3,11 @@ require 'tweet_factory'
 
 describe User do 
   before :each do
-    test_tweets = ["alberta: @bob \"It is remarkable, the character of the pleasure we derive from the best books.\"\n",
-                   "bob: \"They impress us ever with the conviction that one nature wrote and the same reads.\" /cc @alberta\n",
-                   "alberta: hey @christie. what will we be reading at the book club meeting tonight?\n",
-                   "christie: 'Every day, men and women, conversing, beholding and beholden.' /cc @alberta, @bob\n"]
+    test_tweets = ["alberta: @bob \"remarkable.\"\n",
+                   "bob: \"reads.\" /cc @alberta\n",
+                   "alberta: hey @christie.\n",
+                   "spambot: hey @alberta\n",
+                   "christie: 'Every day' /cc @alberta, @bob\n"]
     @test_tweets = TweetFactory.new(test_tweets).create_tweets
     @user = User.new("alberta")
     @user.sent(@test_tweets)
@@ -21,11 +22,22 @@ describe User do
     @user.sent_tweets.count.should == 2
   end
 
-  it "returns 2 as the number of received tweets" do 
-    @user.received_tweets.count.should == 2
+  it "returns 3 as the number of received tweets" do 
+    @user.received_tweets.count.should == 3
   end
 
-  it "returns 4 as the number of tweets involving user" do 
-    @user.tweets_involving_user.count.should == 4
+  it "returns the names of users tweeted to" do 
+    @user.users_tweeted_to.should == ["bob", "christie"]
+  end
+
+  it "returns the names of users tweeted from" do 
+    @user.received_tweets_from.should == ["bob", "spambot", "christie"]
+  end
+
+  it "returns the users interacting with" do 
+    @user.users_tweeted_to
+    @user.received_tweets_from
+
+    @user.interacting_with.should == ["bob", "christie", "bob", "spambot", "christie"]
   end
 end
