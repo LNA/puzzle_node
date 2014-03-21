73c2293c096b3c0f8067d8653b9e342acac98bd6
LNA <LaToyaNAllen@gmail.com>
2014-03-21 15:25:11 -0500

    whitespece

--- micro_blog/spec/parser_spec.rb
+++ micro_blog/spec/parser_spec.rb
@@ -3,11 +3,12 @@ require 'parser'
 describe Parser do 
   let (:tweet_parser) {Parser.new('data/sample_input.txt')}
 
-  it "parses a file" do 
+  it "parses tweets" do 
     tweet_parser.parse_tweets.count.should == 13
   end
 
   it "parses users twitter handles from a txt file" do 
-    tweet_parser.parse_names.should == ["alberta", "bob", "christie", "duncan", "emily", "farid"]
+    tweet_parser.parse_names.should == ["alberta", "bob", "christie", 
+                                        "duncan", "emily", "farid"]
   end
 end
