7afcf3026e20e75acb9dacfbd6b77d7a005fa40c
LNA <LaToyaNAllen@gmail.com>
2014-03-21 15:32:36 -0500

    refactors creation of tweets

--- micro_blog/lib/tweet_factory.rb
+++ micro_blog/lib/tweet_factory.rb
@@ -2,14 +2,19 @@ class TweetFactory
 
   def create(tweet)    
     sender = tweet[/^[^:]*/]
-    receivers = []
-  
-    mid_receivers = tweet.scan(/(@+[a-zA-Z]+)/).flatten
-    
-    mid_receivers.each do |mid_receiver|
-      receivers << mid_receiver[/([a-zA-Z]+)/]
-    end
+    determine_receivers(tweet)
+    Tweet.new(sender, @receivers)
+  end
 
-    Tweet.new(sender, receivers)
+  private
+
+  def determine_receivers(tweet)
+    @receivers = []
+    @mid_receivers = tweet.scan(/(@+[a-zA-Z]+)/).flatten
+    
+    @mid_receivers.each do |mid_receiver|
+      @receivers << mid_receiver[/([a-zA-Z]+)/]
+    end
+    @receivers
   end
 end
